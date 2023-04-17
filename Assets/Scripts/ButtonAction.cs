3397229fd53c4c650795c16ac1483ec293522407
jasonleung1394 <jasonleung1394@gmail.com>
2023-04-17 15:47:26 +1000

    hope

--- Assets/Scripts/ButtonAction.cs
+++ Assets/Scripts/ButtonAction.cs
@@ -61,7 +61,7 @@ public class ButtonAction : MonoBehaviour
         TimeMsg timeMsg = new TimeMsg((uint)0, (uint)0);
         HeaderMsg header = new HeaderMsg(1, timeMsg, "");
         GoalIDMsg goalIDMsg = new GoalIDMsg(timeMsg, "");
-        MoveActionGoalMsg msg_to_publish = new MoveActionGoalMsg(new MoveGoalMsg(0.08, 0.1));
+        MoveActionGoalMsg msg_to_publish = new MoveActionGoalMsg(header, goalIDMsg, new MoveGoalMsg(0.08, 0.1));
         Debug.Log(msg_to_publish);
         ros.Publish("/franka_gripper/move/goal", msg_to_publish);
     }
@@ -72,7 +72,7 @@ public class ButtonAction : MonoBehaviour
         HeaderMsg header = new HeaderMsg(1, timeMsg, "");
         GoalIDMsg goalIDMsg = new GoalIDMsg(timeMsg, "");
         GraspActionGoalMsg msg_to_publish = new GraspActionGoalMsg(
-            new GraspGoalMsg(0.03, new GraspEpsilonMsg(0.005, 0.005), 0.1, 5.0)
+           header, goalIDMsg, new GraspGoalMsg(0.03, new GraspEpsilonMsg(0.005, 0.005), 0.1, 5.0)
         );
         ros.Publish("/franka_gripper/grasp/goal", msg_to_publish);
     }
