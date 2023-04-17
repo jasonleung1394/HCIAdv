57fc6b18187f9ccf253810b6a7a730cbfbde57a6
json <jasonleung1394@gmail.com>
2023-04-18 02:02:30 +1000

    gripper done. need sync method in Unity

--- Assets/RosMessages/FrankaGripper/msg/GraspActionGoalMsg.cs
+++ Assets/RosMessages/FrankaGripper/msg/GraspActionGoalMsg.cs
@@ -13,15 +13,21 @@ namespace RosMessageTypes.FrankaGripper
         public const string k_RosMessageName = "franka_gripper/GraspActionGoal";
         public override string RosMessageName => k_RosMessageName;
 
+        public Std.HeaderMsg header;
+        public Actionlib.GoalIDMsg goal_id;
         public GraspGoalMsg goal;
 
         public GraspActionGoalMsg()
         {
+            this.header = new Std.HeaderMsg();
+            this.goal_id = new Actionlib.GoalIDMsg();
             this.goal = new GraspGoalMsg();
         }
 
-        public GraspActionGoalMsg(GraspGoalMsg goal)
+        public GraspActionGoalMsg(Std.HeaderMsg header, Actionlib.GoalIDMsg goal_id, GraspGoalMsg goal)
         {
+            this.header = header;
+            this.goal_id = goal_id;
             this.goal = goal;
         }
 
@@ -29,17 +35,23 @@ namespace RosMessageTypes.FrankaGripper
 
         private GraspActionGoalMsg(MessageDeserializer deserializer)
         {
+            this.header = Std.HeaderMsg.Deserialize(deserializer);
+            this.goal_id = Actionlib.GoalIDMsg.Deserialize(deserializer);
             this.goal = GraspGoalMsg.Deserialize(deserializer);
         }
 
         public override void SerializeTo(MessageSerializer serializer)
         {
+            serializer.Write(this.header);
+            serializer.Write(this.goal_id);
             serializer.Write(this.goal);
         }
 
         public override string ToString()
         {
             return "GraspActionGoalMsg: " +
+            "\nheader: " + header.ToString() +
+            "\ngoal_id: " + goal_id.ToString() +
             "\ngoal: " + goal.ToString();
         }
 
--- Assets/RosMessages/FrankaGripper/msg/MoveActionGoalMsg.cs
+++ Assets/RosMessages/FrankaGripper/msg/MoveActionGoalMsg.cs
@@ -13,15 +13,21 @@ namespace RosMessageTypes.FrankaGripper
         public const string k_RosMessageName = "franka_gripper/MoveActionGoal";
         public override string RosMessageName => k_RosMessageName;
 
+        public Std.HeaderMsg header;
+        public Actionlib.GoalIDMsg goal_id;
         public MoveGoalMsg goal;
 
         public MoveActionGoalMsg()
         {
+            this.header = new Std.HeaderMsg();
+            this.goal_id = new Actionlib.GoalIDMsg();
             this.goal = new MoveGoalMsg();
         }
 
-        public MoveActionGoalMsg(MoveGoalMsg goal)
+        public MoveActionGoalMsg(Std.HeaderMsg header, Actionlib.GoalIDMsg goal_id, MoveGoalMsg goal)
         {
+            this.header = header;
+            this.goal_id = goal_id;
             this.goal = goal;
         }
 
@@ -29,17 +35,23 @@ namespace RosMessageTypes.FrankaGripper
 
         private MoveActionGoalMsg(MessageDeserializer deserializer)
         {
+            this.header = Std.HeaderMsg.Deserialize(deserializer);
+            this.goal_id = Actionlib.GoalIDMsg.Deserialize(deserializer);
             this.goal = MoveGoalMsg.Deserialize(deserializer);
         }
 
         public override void SerializeTo(MessageSerializer serializer)
         {
+            serializer.Write(this.header);
+            serializer.Write(this.goal_id);
             serializer.Write(this.goal);
         }
 
         public override string ToString()
         {
             return "MoveActionGoalMsg: " +
+            "\nheader: " + header.ToString() +
+            "\ngoal_id: " + goal_id.ToString() +
             "\ngoal: " + goal.ToString();
         }
 
