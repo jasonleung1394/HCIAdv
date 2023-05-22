65ca452a3d683cfd1e3281d6282d65ba7588f770
jason-Ubuntu20.04-RT <jasonleung1394@gmail.com>
2023-05-22 18:06:06 +1000

    changed torque threshhold config

--- franka_example_controllers/scripts/collision_behavior_node.py
+++ franka_example_controllers/scripts/collision_behavior_node.py
@@ -9,7 +9,7 @@ def collision_behavior_node():
     set_collision_behavior_client = ros.ServiceProxy('/franka_control/set_force_torque_collision_behavior', SetForceTorqueCollisionBehavior)
     request = SetForceTorqueCollisionBehaviorRequest()
     request.lower_torque_thresholds_nominal = [0.05] * 7     #Nm
-    request.upper_torque_thresholds_nominal = [20,20,20,40,20,20,20]     
+    request.upper_torque_thresholds_nominal = [20,40,20,40,20,20,20]     
     request.lower_force_thresholds_nominal = [2.0] * 6   
     request.upper_force_thresholds_nominal = [20] * 6 
 
@@ -26,7 +26,7 @@ def collision_behavior_node():
 def checkCollision(data):
     joint_contact = data.joint_contact
     joint_collision = data.joint_collision
-    if 1 in joint_collision: ros.signal_shutdown("collision")
+    # if 1 in joint_collision: ros.loginfo(joint_collision)
 
 if __name__ == '__main__':
     ros.init_node('collision_behavior_node',disable_signals=True)
